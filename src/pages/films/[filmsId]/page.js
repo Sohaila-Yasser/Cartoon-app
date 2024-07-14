c168451979252c9f4bcedc5a888bb5edb2673abc
Sohaila Yasser <129595248+Sohaila-Yasser@users.noreply.github.com>
2024-07-14 23:10:24 +0300

    Update page.js

--- src/pages/films/[filmsId]/page.js
+++ src/pages/films/[filmsId]/page.js
@@ -72,7 +72,7 @@ const FilmDetailPage = () => {
           <p><span className="bold">IMDB Votes:</span> {filmDetails.imdbVotes}</p>
           <p><span className="bold">Type:</span> {filmDetails.Type}</p>
           <p><span className="bold">DVD:</span> {filmDetails.DVD}</p>
-          <p><span className="bold">Website:</span><a href="https://spacetoon.com">https://spacetoon.com</a>/</p>
+          <p><span className="bold">Website:</span> <a href="https://spacetoon.com">https://spacetoon.com</a>/</p>
       </div>
     </div>
   );
